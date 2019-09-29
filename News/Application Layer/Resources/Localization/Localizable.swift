8932b62de0fbb5379cf88313f61b608d48bf504a
Vladislav <vladislavmarkov18@gmail.com>
2019-09-29 11:38:19 +0300

    Remove AppLocalizationManager (it was not used)

--- News/Application Layer/Resources/Localization/Localizable.swift
+++ News/Application Layer/Resources/Localization/Localizable.swift
@@ -17,11 +17,6 @@ extension Localizable {
     }
     
     var localized: String {
-        let lang: String = AppLocalizationManager.current.type.localizationKey
-        if let path = Bundle.main.path(forResource: lang, ofType: "lproj"), let bundle = Bundle(path: path) {
-            return NSLocalizedString(self.key, tableName: nil, bundle: bundle, value: "", comment: "")
-        } else {
-            return NSLocalizedString(self.key, comment: "")
-        }
+        return NSLocalizedString(self.key, comment: "")
     }
 }
