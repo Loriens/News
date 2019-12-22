b0f47574d4768f7bb7c19e81cb5e5f3da4f7ad82
Vladislav Markov <vladislavmarkov18@gmail.com>
2019-12-22 13:41:34 +0300

    Fix

--- News/Application Layer/AppDelegate.swift
+++ News/Application Layer/AppDelegate.swift
@@ -23,16 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) { }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) { }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) { }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) { }
-    
-    func applicationWillTerminate(_ application: UIApplication) { }
-    
     // MARK: - Application configuration
     private func setup(_ application: UIApplication) { }
     
