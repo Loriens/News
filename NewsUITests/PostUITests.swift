7b19a3b0a6d9aa139b3f128e196adfc2e921450e
Vladislav Markov <vladislavmarkov18@gmail.com>
2020-05-17 17:32:06 +0300

    Add the first UI test

---
+++ NewsUITests/PostUITests.swift
@@ -0,0 +1,41 @@
+//
+//  PostUITests.swift
+//  NewsUITests
+//
+//  Created by Vladislav on 17.05.2020.
+//  Copyright © 2020 Vladislav Markov. All rights reserved.
+//
+
+import XCTest
+
+class PostUITests: XCTestCase {
+    
+    // MARK: - Props
+    var app: XCUIApplication!
+
+    // MARK: - Setup functions
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+
+    // MARK: - Test functions
+    func testOpeningFirstPost() throws {
+        let cell = app.tables.staticTexts.firstMatch
+        waitForElementToAppear(element: cell)
+        cell.tap()
+        XCTAssertEqual(app.staticTexts.firstMatch.label, "Detail")
+    }
+    
+    // MARK: - Module functions
+    func waitForElementToAppear(element: XCUIElement, timeout: TimeInterval = 10) {
+        let existsPredicate = NSPredicate(format: "exists == 1")
+        let expectation = self.expectation(for: existsPredicate, evaluatedWith: element, handler: nil)
+        let result = XCTWaiter.wait(for: [expectation], timeout: timeout)
+        
+        XCTAssertEqual(result, XCTWaiter.Result.completed)
+    }
+
+}
