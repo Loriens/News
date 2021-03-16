4073d343b11232c4c138ff6bb0a86b68824adefb
Vladislav Markov <vladislavmarkov18@gmail.com>
2021-03-16 22:31:48 +0300

    Remove unused ci.sh

--- ci.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-xcodebuild -project 'News.xcodeproj' -scheme 'News' -destination 'platform=iOS Simulator,name=iPhone 8' test
-xcodebuild -project 'News.xcodeproj' -scheme 'News' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
